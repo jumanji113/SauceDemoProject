b226b2a90c216ff4aea93b0b2b3a5f1b678ca969
Alex <udinkik@mail.ru>
2025-04-28 17:38:03 +0300

    init SumTest

--- src/main/java/yudin/pages/MainPage.java
+++ src/main/java/yudin/pages/MainPage.java
@@ -8,8 +8,7 @@ import org.junit.jupiter.api.Assertions;
 import yudin.constans.ItemEnum;
 
 import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class MainPage {
 
@@ -20,12 +19,43 @@ public class MainPage {
     private Map<ItemEnum, SelenideElement> itemLocators = new HashMap<>();
 
     public MainPage() {
-        itemLocators.put(ItemEnum.BACKPACK, Selenide.$x("//button[@id='add-to-cart-sauce-labs-backpack']"));
-        itemLocators.put(ItemEnum.BIKE_LIGHT, Selenide.$x("//button[@id='add-to-cart-sauce-labs-bike-light']"));
-        itemLocators.put(ItemEnum.BOLT_TSHIRT, Selenide.$x("//button[@id='add-to-cart-sauce-labs-bolt-t-shirt']"));
-        itemLocators.put(ItemEnum.FLEECE_JACKET, Selenide.$x("//button[@id='add-to-cart-sauce-labs-fleece-jacket']"));
-        itemLocators.put(ItemEnum.ONESIE, Selenide.$x("//button[@id='add-to-cart-sauce-labs-onesie']"));
-        itemLocators.put(ItemEnum.T_SHIRT_RED, Selenide.$x("//button[@id='add-to-cart-test.allthethings()-t-shirt-(red)']"));
+        itemLocators.put(ItemEnum.BACKPACK, Selenide.$x("//img[@alt='Sauce Labs Backpack']/../../.."));
+        itemLocators.put(ItemEnum.BIKE_LIGHT, Selenide.$x("//img[@alt='Sauce Labs Bike Light']/../../.."));
+        itemLocators.put(ItemEnum.BOLT_TSHIRT, Selenide.$x("//img[@alt='Sauce Labs Bolt T-Shirt']/../../.."));
+        itemLocators.put(ItemEnum.FLEECE_JACKET, Selenide.$x("//img[@alt='Sauce Labs Fleece Jacket']/../../.."));
+        itemLocators.put(ItemEnum.ONESIE, Selenide.$x("//img[@alt='Sauce Labs Onesie']/../../.."));
+        itemLocators.put(ItemEnum.T_SHIRT_RED, Selenide.$x("//img[@alt='Test.allTheThings() T-Shirt (Red)']/../../.."));
+    }
+
+    @Step
+    public String randomItemAddToCart(){
+        List<Map.Entry<ItemEnum, SelenideElement>> entries = new ArrayList<>(itemLocators.entrySet());
+        Random random = new Random();//класс рандом инициализируем
+        int randomIndex = random.nextInt(entries.size());//получаем рандомное число по размеру листа
+        clickAddButtonToCart(entries.get(randomIndex).getKey());//кликаем на кнопку добавить
+        return getPriceItems(entries.get(randomIndex).getKey()).substring(1);//получаем строчку, которую в mainpage преобразуем в double, можно исправить
+    }
+
+    @Step("Получение цены {itemEnum}")
+    public String getPriceItems(ItemEnum itemEnum) {
+        SelenideElement itemLocator = itemLocators.get(itemEnum);
+        if (itemLocator == null) {
+            throw new IllegalArgumentException("Элемент не найден");
+        }
+
+        SelenideElement priceElement = itemLocator.$x(".//div[@data-test='inventory-item-price']");
+        System.out.println(priceElement.getText().trim());
+        return priceElement.getText().trim();
+    }
+
+    @Step("Добавление товара в корзину {itemEnum}")
+    public void clickAddButtonToCart(ItemEnum itemEnum) {
+        SelenideElement itemLocator = itemLocators.get(itemEnum);
+        if (itemLocator == null) {
+            throw new IllegalArgumentException("Элемент не найден");
+        }
+        SelenideElement button = itemLocator.$x(".//button[text() = 'Add to cart']");
+        button.click();
     }
 
     @Step("Проверка лого сайта")
@@ -76,16 +106,6 @@ public class MainPage {
         int actualCounter = actualCounterText.isEmpty() ? 0 : Integer.parseInt(actualCounterText);
         // Проверяем, что счетчик обновился
         Assertions.assertEquals(expectedCounter, actualCounter, "Счетчик корзины не обновился");
-        return this;
-    }
-
-    @Step("Добавление элементов в корзину без проверки счетчика")
-    public MainPage addItemToCart(String shortName) {
-        SelenideElement button = itemLocators.get(shortName);
-        if (button == null) {
-            throw new IllegalArgumentException("Элемент не найден");
-        }
-        button.click();
         return this;
     }
 
